e4555d0079a09bcb91601ec4703b2bdb5562eacd
Gabriel Antonio Fernandez <gafernan@sfu.ca>
2021-02-05 23:50:19 -0800

    Added some initial skeleton files

---
+++ character.py
@@ -0,0 +1,9 @@
+#python character defnition for game engine
+#created by Gabriel Antonio Fernandez
+
+
+#define game character
+class character:
+    speed = 5           #speed of the character
+    dead = False        #death state
+    vulnerability = 2   #vulnerability (lower means less vulnerable)
---
+++ engine.py
@@ -0,0 +1,14 @@
+#python game engine
+#created by Gabriel Antonio Fernandez
+import character.py
+
+class engine:
+    #load game entities method
+    def
+
+    #load control mappings method
+
+    #game start method
+
+    #game quit method
+
